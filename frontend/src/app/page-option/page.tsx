b17f5b4bcbd9e2c8f87deea6293e74b311435c96
--replace-all <brunotorresreal@gmail.com>
2025-10-12 11:48:20 -0300

    feat: install MUI and MUI icons

--- frontend/src/app/page-option/page.tsx
+++ frontend/src/app/page-option/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { Button, Typography } from "@mui/material";
+
 export default function PageOption() {
-  return <div>My Page</div>;
+   return (
+    <div style={{ padding: 20 }}>
+      <Typography variant="h4" gutterBottom>
+        MUI + Next.js funcionando!
+      </Typography>
+      <Button variant="contained" color="primary">
+        Clique aqui
+      </Button>
+    </div>
+  );
 }
